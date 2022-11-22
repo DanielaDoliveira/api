13e2204f73d77950f9ec5d38184cac68feac4af8
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-22 12:42:58 -0300

    Login e senha

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "root",
+  database: "projeto_integrado",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const authMiddleware = require("./middlewares/auth");
+
+const UserController = require("./controllers/UserController");
+const RankingBoardController = require("./controllers/RankingBoardController");
+const SessionController = require("./controllers/SessionController");
+const PointsController = require("./controllers/PointsController");
+
+const routes = express.Router();
+
+routes.post("/newuser", UserController.store);
+routes.post("/sessions", SessionController.store);
+routes.use(authMiddleware);
+routes.get("/ranking", RankingBoardController.index);
+routes.put("/users", UserController.update);
+routes.put("/user/rankup", PointsController.update);
+routes.delete("/delete/user", UserController.destroy);
+module.exports = routes;
