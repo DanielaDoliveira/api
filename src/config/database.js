7c419d14dc1c3f0995f896c0d64cf277b5de7f6a
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-23 11:46:27 -0300

    Alteracao no arquivo database

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,14 @@
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "root",
-  database: "projeto_integrado",
+  host: " dpg-cdujotta49967v5r3770-a",
+  username: "root",
+  password: "projetointegrado",
+  database: "projeto_integrado_80vr",
   define: {
     timestamps: true,
     underscored: true,
   },
 };
+//username antigo eraq postgres
+//postgres://USER:PASSWORD@EXTERNAL_HOST:PORT/DATABASE
+//postgres://root:NYCvpOd95YHjLcjPMxBD78GTe9h3ORxe@dpg-cdujotta49967v5r3770-a.oregon-postgres.render.com/projeto_integrado_80vr
